426a3ab6ac628269821377abf420d51b416f6f25
Abhishek Srinath <asrinath@buffalo.edu>
2012-03-11 00:37:13 -0500

    Added the frame work for implementing the GBN

---
+++ GBN/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=gbn
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/gbn
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=gbn.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/gbn.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=gbn
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/gbn
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=gbn.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/gbn.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
